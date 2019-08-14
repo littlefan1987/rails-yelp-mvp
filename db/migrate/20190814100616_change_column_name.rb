947e01fd702c380e1f7ed2851e322c1c886e86d5
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-08-14 12:10:38 +0200

    migration

--- db/migrate/20190814100616_change_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[5.2]
-  def change
-    rename_column(:restaurants, :phone, :phone_number)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_092600) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.string "phone"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
